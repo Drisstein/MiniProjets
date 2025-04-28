2cb9d6fbcea4ac5044a08ffd7c3dafb7b0be9e62
drisstein <idrissahmad1903@gmail.com>
2025-04-28 20:12:59 +0200

    Fin de l'exercice 2 !

--- TD1-PHP/EXAM2023/EXO2.php
+++ TD1-PHP/EXAM2023/EXO2.php
@@ -1,20 +1,30 @@
-<?PHP
+<?php
 session_start();
+// 1- Tableau de base
 $prenoms = array('aziz', 'niang', 'khadija', 'amdi');
-// 1-Sauvegarder dans $_SESSION avec leur ID
-foreach($prenoms as $id => $prenom) { {
-        $_SESSION['prenoms'][$id + 1] = $prenom; // Sauvegarder dans $_SESSION
-    }
 
+
+// 2- Sauvegarder dans $_SESSION avec leur ID
+foreach($prenoms as $id => $prenom) {
+    $_SESSION[$id + 1] = $prenom;
 }
-
-// 2-Afficher un tableau HTML avec les valeurs stockées !
-echo"<table border='1' cellpadding='5' cellspacing='0'>";
-echo"<tr><th>N° </th><th>Prénom</th></tr>";
-foreach($_SESSION as $key =>$value) {
+// 3- Afficher tableau HTML avec les valeurs de $_SESSION
+echo "<h2>Tableau \$SESSION</h2><hr>";
+echo "<table border='1' cellpadding='5' cellspacing='0'>";
+echo "<tr style='background-color:lightblue'><th>N°</th><th>Prénom</th></tr>";
+foreach($_SESSION as $key => $value) {
     echo "<tr><td>$key</td><td>$value</td></tr>";
-    
 }
-session_destroy();
+echo "</table>";
 
+echo "<br><br>";
+
+// 4- Afficher maintenant le tableau de base $prenoms
+echo "<h2>Tableau \$prenoms</h2><hr>";
+foreach($prenoms as $key => $prenom) {
+    echo "N°".($key + 1).":" .$prenom . "<br>";
+}
+
+// 5- Détruire la session seulement à la toute fin
+session_destroy();
 ?>
